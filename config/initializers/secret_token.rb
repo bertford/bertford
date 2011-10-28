07ba7421858ee8db149d692565d7e145a97f4e6c
Bert Ford <git@bertfordseo.com>
2011-10-28 15:26:55 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bertford::Application.config.secret_token = '285fc7ef65711cce05a3b378e57994ce482e2a507d7ce2a1388b74abe75180c9f5131ddf4733070de88f92659a91a467faff7bdb199b341d4b00bc9fef9b2e7a'
